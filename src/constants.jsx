4a0c891e01d638c13a6a9b15fd65bea11073407f
maitrishah101 <maitri.shah101@gmail.com>
2022-03-27 14:25:47 -0700

    Update constants.jsx

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import './constants.css';
-
+import instagram from './img/instagram.png'
+import twitter from './img/twitter.png'
+import linkedin from './img/linkedin.png'
 
 export const SHORTBIO = 
-<div class="bio" style={{marginTop: "7%"}}>
+<div class="bio" style={{marginTop: "10%"}}>
     <div style={{marginBottom: "3%"}}>
-        An engineer + advocate for womxn in tech by day.
+        I'm an engineer, designer, and granola girl. 
     </div>
     <div style={{marginBottom: "3%"}}>
-        A designer + digital creator by night.
+        I love to tweet, travel, read, cook, exercise, and create.
     </div>
-    <div style={{marginBottom: "3%"}}>
-        I strive to create more than I consume.
-    </div>
-    <div style={{marginBottom: "3%"}}>
-        I see opportunities to make an impact, in all things.
-    </div>
-    <div style={{marginBottom: "3%"}}>
-        Get discounts on some of my favorite products <a class="bio" href="http://linktr.ee/maitrishahhhh">here</a>. 
+    <div style={{marginBottom: "5%"}}>
+        I'm passionate about design, sustainability, healthy living, and entrepreneurship. 
     </div>
     <div class="socialicons" style={{marginBottom: "3%"}}>
-        <a href="https://www.instagram.com/maitrishahhhh/"><img class="social" src={require("./img/instagram.png")} width="7%" height="7%" /></a>
-        <a href="https://twitter.com/maitrishahhhh"><img class="social" src={require("./img/twitter.png")} width="7%" height="7%" /></a>
-        <a href="https://www.linkedin.com/in/maitri-shah/"><img class="social" src={require("./img/linkedin.png")} width="7%" height="7%" /></a>
+        <a href="https://www.instagram.com/maitrishahhhh/"><img class="social" src={instagram} width="7%" height="7%" /></a>
+        <a href="https://twitter.com/maitrishahhhh"><img class="social" src={twitter} width="7%" height="7%" /></a>
+        <a href="https://www.linkedin.com/in/maitri-shah/"><img class="social" src={linkedin} width="7%" height="7%" /></a>
     </div>
 </div>
